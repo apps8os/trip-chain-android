4dd0fc64e51afbf7d1431137139acf054cc8e8ee
Jukka-Pekka Virtanen <jukkapekka.virtanen@gmail.com>
2014-02-11 00:29:30 +0200

    Discarding tilting.

--- client/src/fi/aalto/tripchain/Route.java
+++ client/src/fi/aalto/tripchain/Route.java
@@ -7,10 +7,19 @@ import android.location.Location;
 
 public class Route {
 	private List<RouteSegment> route = new ArrayList<RouteSegment>();
+	private Location lastLocation;
 	
 	public void onActivity(Activity activity) {
+		if (activity == Activity.TILTING) {
+			return;
+		}
+		
 	    if (route.size() == 0) {
 	    	route.add(new RouteSegment(activity));
+	    	if (lastLocation != null) {
+	    		onLocation(lastLocation);
+	    		lastLocation = null;
+	    	}
 	    } else {
 	    	RouteSegment lastSegment = route.get(route.size() - 1);
 	    	if (lastSegment.activity != activity) {
@@ -21,6 +30,7 @@ public class Route {
 	
 	public void onLocation(Location location) {
 	    if (route.size() == 0) {
+	    	lastLocation = location;
 	    	return;
 	    }
 	    
