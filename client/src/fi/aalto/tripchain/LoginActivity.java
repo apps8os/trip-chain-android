99b1db75487747fce286759aca0445c9cb946dca
Jukka-Pekka Virtanen <jukkapekka.virtanen@gmail.com>
2014-03-02 15:40:46 +0200

    Login using google account.

--- client/src/fi/aalto/tripchain/LoginActivity.java
+++ client/src/fi/aalto/tripchain/LoginActivity.java
@@ -1,25 +1,37 @@
 package fi.aalto.tripchain;
 
-import java.util.UUID;
+import java.io.InputStream;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+import org.json.JSONObject;
+
+import com.google.android.gms.auth.GoogleAuthUtil;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.accounts.AccountManagerCallback;
 import android.accounts.AccountManagerFuture;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class LoginActivity extends Activity {
-	
+
 	private static final String TAG = LoginActivity.class.getSimpleName();
 
 	private static final String KEY_USER = "auth_user";
 	private static final String KEY_TOKEN = "auth_token";
-	
+	private static final String KEY_LOGIN_ID = "login_id";
+
 	private static final String SCOPE = "profile";
 
 	private SharedPreferences preferences;
@@ -31,37 +43,94 @@ public class LoginActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		accountManager = AccountManager.get(this);
 
 		preferences = getSharedPreferences(Configuration.SHARED_PREFERENCES,
 				MODE_MULTI_PROCESS);
-		String loginId = preferences.getString("login_id", null);
+		String loginId = preferences.getString(KEY_LOGIN_ID, null);
 		if (loginId != null) {
 			startMain();
-		} else {
-			chooseAccount();
+			return;
 		}
+
+		setContentView(R.layout.activity_login);
 	}
 
-	private void chooseAccount() {
+	public void chooseAccount(View _) {
 		// use https://github.com/frakbot/Android-AccountChooser for
 		// compatibility with older devices
 		Intent intent = AccountManager.newChooseAccountIntent(null, null,
-				new String[] { "com.google" }, true, null, null, null, null);
+				new String[] { "com.google" }, false, null, null, null, null);
 		startActivityForResult(intent, ACCOUNT_CODE);
 	}
-	
-	private void doCoolAuthenticatedStuff() {
-		// TODO: insert cool stuff with authPreferences.getToken()
-		 
+
+	private void getUserId() {
 		Log.e(TAG, getToken());
+		final ProgressDialog dialog = ProgressDialog.show(this, "",
+				"Loading. Please wait...", true);
+
+		new AsyncTask<Void, Void, Boolean>() {
+			protected Boolean doInBackground(Void... _) {
+				try {
+					String token = getToken();
+
+					URL url = new URL(
+							"https://www.googleapis.com/oauth2/v1/userinfo?access_token="
+									+ token);
+					HttpURLConnection con = (HttpURLConnection) url
+							.openConnection();
+					int serverCode = con.getResponseCode();
+					// successful query
+					if (serverCode == 200) {
+						InputStream is = con.getInputStream();
+
+						String message = new Scanner(is, "UTF-8").useDelimiter(
+								"\\A").next();
+						JSONObject j = new JSONObject(message);
+						is.close();
+
+						Editor editor = preferences.edit();
+						editor.putString(KEY_LOGIN_ID, j.getString("id"));
+						editor.commit();
+
+						return true;
+						// bad token, invalidate and get a new one
+					} else if (serverCode == 401) {
+						GoogleAuthUtil.invalidateToken(LoginActivity.this,
+								token);
+						Log.e(TAG,
+								"Server auth error: "
+										+ new Scanner(con.getErrorStream(),
+												"UTF-8").useDelimiter("\\A")
+												.next());
+						// unknown error, do something else
+					} else {
+						Log.e(TAG, "Server returned the following error code: "
+								+ serverCode, null);
+					}
+
+				} catch (Exception e) {
+					Log.d(TAG, "failed to get user id", e);
+				}
+
+				return false;
+			}
+
+			protected void onPostExecute(Boolean success) {
+				Log.d(TAG, "Cancelling dialog.");
+				dialog.cancel();
+
+				if (success) {
+					startMain();
+				}
+			}
+		}.execute();
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		Log.d(TAG, "onActivityResult");
-		
+
 		if (resultCode != RESULT_OK) {
 			return;
 		}
@@ -71,9 +140,10 @@ public class LoginActivity extends Activity {
 			requestToken();
 		} else if (requestCode == ACCOUNT_CODE) {
 
-			String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+			String accountName = data
+					.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
 			setUser(accountName);
-			
+
 			Log.d(TAG, "ACCOUNT_CODE " + accountName);
 
 			// invalidate old tokens which might be cached. we want a fresh
@@ -94,10 +164,11 @@ public class LoginActivity extends Activity {
 				break;
 			}
 		}
-		
+
 		Log.d(TAG, "Request token. " + (userAccount != null));
 
-		accountManager.getAuthToken(userAccount, "oauth2:" + SCOPE, null, this, new OnTokenAcquired(), null);
+		accountManager.getAuthToken(userAccount, "oauth2:" + SCOPE, null, this,
+				new OnTokenAcquired(), null);
 	}
 
 	private void invalidateToken() {
@@ -138,7 +209,7 @@ public class LoginActivity extends Activity {
 		@Override
 		public void run(AccountManagerFuture<Bundle> result) {
 			Log.d(TAG, "onTokenAcquired");
-			
+
 			try {
 				Bundle bundle = result.getResult();
 
@@ -146,16 +217,16 @@ public class LoginActivity extends Activity {
 				if (launch != null) {
 					startActivityForResult(launch, AUTHORIZATION_CODE);
 				} else {
-					String token = bundle.getString(AccountManager.KEY_AUTHTOKEN);
+					String token = bundle
+							.getString(AccountManager.KEY_AUTHTOKEN);
 
 					setToken(token);
 
-					doCoolAuthenticatedStuff();
+					getUserId();
 				}
 			} catch (Exception e) {
-				throw new RuntimeException(e);
+
 			}
 		}
 	}
-
 }
