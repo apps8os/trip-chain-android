616debf73e7691f0cf9a06fb4cbfb39c99009a5f
Jukka-Pekka Virtanen <jukkapekka.virtanen@gmail.com>
2014-03-04 22:20:25 +0200

    Login now implemented correctly using the new Google+ API.

--- client/src/fi/aalto/tripchain/LoginActivity.java
+++ client/src/fi/aalto/tripchain/LoginActivity.java
@@ -1,49 +1,138 @@
 package fi.aalto.tripchain;
 
-import java.io.InputStream;
+import com.google.android.gms.common.ConnectionResult;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
-import org.json.JSONObject;
+import com.google.android.gms.plus.*;
+import com.google.android.gms.plus.model.people.Person;
 
-import com.google.android.gms.auth.GoogleAuthUtil;
-
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.accounts.AccountManagerCallback;
-import android.accounts.AccountManagerFuture;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
-import android.os.AsyncTask;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements OnClickListener, ConnectionCallbacks,
+		OnConnectionFailedListener {
+
+	/* Request code used to invoke sign in user interactions. */
+	private static final int RC_SIGN_IN = 0;
+
+	/* Client used to interact with Google APIs. */
+	private GoogleApiClient mGoogleApiClient;
+
+	/*
+	 * A flag indicating that a PendingIntent is in progress and prevents us
+	 * from starting further intents.
+	 */
+	private boolean mIntentInProgress;
 
 	private static final String TAG = LoginActivity.class.getSimpleName();
 
-	private static final String KEY_USER = "auth_user";
-	private static final String KEY_TOKEN = "auth_token";
 	private static final String KEY_LOGIN_ID = "login_id";
 
-	private static final String SCOPE = "profile";
-
 	private SharedPreferences preferences;
-	private AccountManager accountManager;
 
-	private static final int AUTHORIZATION_CODE = 1993;
-	private static final int ACCOUNT_CODE = 1601;
+	/*
+	 * Track whether the sign-in button has been clicked so that we know to
+	 * resolve all issues preventing sign-in without waiting.
+	 */
+	private boolean mSignInClicked;
+
+	/*
+	 * Store the connection result from onConnectionFailed callbacks so that we
+	 * can resolve them when the user clicks sign-in.
+	 */
+	private ConnectionResult mConnectionResult;
+
+	/* A helper method to resolve the current ConnectionResult error. */
+	private void resolveSignInError() {
+		if (mConnectionResult.hasResolution()) {
+			try {
+				mIntentInProgress = true;
+				mConnectionResult.startResolutionForResult(this, RC_SIGN_IN);
+			} catch (SendIntentException e) {
+				// The intent was canceled before it was sent. Return to the
+				// default
+				// state and attempt to connect to get an updated
+				// ConnectionResult.
+				mIntentInProgress = false;
+				mGoogleApiClient.connect();
+			}
+		}
+	}
+
+	public void onConnectionFailed(ConnectionResult result) {
+		if (!mIntentInProgress) {
+			// Store the ConnectionResult so that we can use it later when the
+			// user clicks
+			// 'sign-in'.
+			mConnectionResult = result;
+
+			if (mSignInClicked) {
+				// The user has already clicked 'sign-in' so we attempt to
+				// resolve all
+				// errors until the user is signed in, or they cancel.
+				resolveSignInError();
+			}
+		}
+	}
+
+	public void onClick(View view) {
+		if (view.getId() == R.id.sign_in_button && !mGoogleApiClient.isConnecting()) {
+			mSignInClicked = true;
+			resolveSignInError();
+		}
+	}
+
+	protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+		if (requestCode == RC_SIGN_IN) {
+			if (responseCode != RESULT_OK) {
+				mSignInClicked = false;
+			}
+
+			mIntentInProgress = false;
+
+			if (!mGoogleApiClient.isConnecting()) {
+				mGoogleApiClient.connect();
+			}
+		}
+	}
+
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		mSignInClicked = false;
+		Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
+
+		if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+			Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+			String personName = currentPerson.getDisplayName();
+			Toast.makeText(this, personName + " " + currentPerson.getId(), Toast.LENGTH_LONG).show();
+			Log.i(TAG, personName + " " + currentPerson.getId());
+			
+			Editor e = preferences.edit();
+			e.putString(KEY_LOGIN_ID, currentPerson.getId());
+			e.commit();
+			
+
+	        Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+			startMain();
+		}
+
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		accountManager = AccountManager.get(this);
 
 		preferences = getSharedPreferences(Configuration.SHARED_PREFERENCES, MODE_MULTI_PROCESS);
 		String loginId = preferences.getString(KEY_LOGIN_ID, null);
@@ -52,135 +141,25 @@ public class LoginActivity extends Activity {
 			return;
 		}
 
+		mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this).addApi(Plus.API, null).addScope(Plus.SCOPE_PLUS_LOGIN)
+				.build();
+
 		setContentView(R.layout.activity_login);
+		findViewById(R.id.sign_in_button).setOnClickListener(this);
 	}
 
-	public void chooseAccount(View _) {
-		Intent intent = AccountManager.newChooseAccountIntent(null, null, new String[] { "com.google" },
-				false, null, null, null, null);
-		startActivityForResult(intent, ACCOUNT_CODE);
+	protected void onStart() {
+		super.onStart();
+		mGoogleApiClient.connect();
 	}
 
-	private void getUserId() {
-		Log.e(TAG, getToken());
-		final ProgressDialog dialog = ProgressDialog.show(this, "", "Loading. Please wait...", true);
+	protected void onStop() {
+		super.onStop();
 
-		new AsyncTask<Void, Void, Boolean>() {
-			protected Boolean doInBackground(Void... _) {
-				try {
-					String token = getToken();
-
-					URL url = new URL("https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + token);
-					HttpURLConnection con = (HttpURLConnection) url.openConnection();
-					int serverCode = con.getResponseCode();
-					// successful query
-					if (serverCode == 200) {
-						InputStream is = con.getInputStream();
-
-						String message = new Scanner(is, "UTF-8").useDelimiter("\\A").next();
-						JSONObject j = new JSONObject(message);
-						is.close();
-
-						Editor editor = preferences.edit();
-						editor.putString(KEY_LOGIN_ID, j.getString("id"));
-						editor.commit();
-
-						GoogleAuthUtil.invalidateToken(LoginActivity.this, token);
-
-						return true;
-						// bad token, invalidate and get a new one
-					} else if (serverCode == 401) {
-						GoogleAuthUtil.invalidateToken(LoginActivity.this, token);
-						Log.e(TAG, "Server auth error: "
-								+ new Scanner(con.getErrorStream(), "UTF-8").useDelimiter("\\A").next());
-						// unknown error, do something else
-					} else {
-						Log.e(TAG, "Server returned the following error code: " + serverCode, null);
-					}
-
-				} catch (Exception e) {
-					Log.d(TAG, "failed to get user id", e);
-				}
-
-				return false;
-			}
-
-			protected void onPostExecute(Boolean success) {
-				Log.d(TAG, "Cancelling dialog.");
-				dialog.cancel();
-
-				if (success) {
-					startMain();
-				}
-			}
-		}.execute();
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		Log.d(TAG, "onActivityResult");
-
-		if (resultCode != RESULT_OK) {
-			return;
+		if (mGoogleApiClient.isConnected()) {
+			mGoogleApiClient.disconnect();
 		}
-
-		if (requestCode == AUTHORIZATION_CODE) {
-			Log.d(TAG, "AUTHORIZATION_CODE");
-			requestToken();
-		} else if (requestCode == ACCOUNT_CODE) {
-
-			String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-			setUser(accountName);
-
-			Log.d(TAG, "ACCOUNT_CODE " + accountName);
-
-			// invalidate old tokens which might be cached. we want a fresh
-			// one, which is guaranteed to work
-			invalidateToken();
-
-			requestToken();
-		}
-	}
-
-	private void requestToken() {
-		Account userAccount = null;
-		String user = getUser();
-		for (Account account : accountManager.getAccountsByType("com.google")) {
-			if (account.name.equals(user)) {
-				userAccount = account;
-
-				break;
-			}
-		}
-
-		Log.d(TAG, "Request token. " + (userAccount != null));
-
-		accountManager.getAuthToken(userAccount, "oauth2:" + SCOPE, null, this, new OnTokenAcquired(), null);
-	}
-
-	private void invalidateToken() {
-		accountManager.invalidateAuthToken("com.google", getToken());
-		setToken(null);
-	}
-
-	public void setUser(String user) {
-		Editor editor = preferences.edit();
-		editor.putString(KEY_USER, user);
-		editor.commit();
-	}
-
-	public void setToken(String password) {
-		Editor editor = preferences.edit();
-		editor.putString(KEY_TOKEN, password);
-		editor.commit();
-	}
-
-	public String getUser() {
-		return preferences.getString(KEY_USER, null);
-	}
-
-	public String getToken() {
-		return preferences.getString(KEY_TOKEN, null);
 	}
 
 	private void startMain() {
@@ -191,28 +170,8 @@ public class LoginActivity extends Activity {
 		finish();
 	}
 
-	private class OnTokenAcquired implements AccountManagerCallback<Bundle> {
-
-		@Override
-		public void run(AccountManagerFuture<Bundle> result) {
-			Log.d(TAG, "onTokenAcquired");
-
-			try {
-				Bundle bundle = result.getResult();
-
-				Intent launch = (Intent) bundle.get(AccountManager.KEY_INTENT);
-				if (launch != null) {
-					startActivityForResult(launch, AUTHORIZATION_CODE);
-				} else {
-					String token = bundle.getString(AccountManager.KEY_AUTHTOKEN);
-
-					setToken(token);
-
-					getUserId();
-				}
-			} catch (Exception e) {
-
-			}
-		}
+	@Override
+	public void onConnectionSuspended(int cause) {
+		mGoogleApiClient.connect();
 	}
 }
